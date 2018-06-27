2db8b033cb02689d8b3ed96aed376a5969dd58ce
Xin Zhang <xzhang@pivotal.io>
2018-06-27 16:14:01 -0700

    Fix variable reference
    Co-authored-by: Xin Zhang <xzhang@pivotal.io>
    Co-authored-by: Bradford Boyle <bboyle@pivotal.io>

--- gp.sh
+++ gp.sh
@@ -9,14 +9,14 @@ docker cp $(docker ps -qa | head -n1):/usr/local/gpdb/lib/postgresql/regress.so 
 docker rm $(docker ps -qa | head -n1)
 # build deploy
 new_tag=greenplum-for-kubernetes:hackday2018.$(pwgen 8 1)
-docker build deploy/ -t $(new_tag)
+docker build deploy/ -t ${new_tag}
 # stop gpdb
 kubectl exec -it master ssh master -- gpstop -a
 # refresh image
-kubectl set image pod/master gpdb=$(new_tag)
-kubectl set image pod/standby gpdb=$(new_tag)
-kubectl set image pod/segment-0a gpdb=$(new_tag)
-kubectl set image pod/segment-0b gpdb=$(new_tag)
+kubectl set image pod/master gpdb=${new_tag}
+kubectl set image pod/standby gpdb=${new_tag}
+kubectl set image pod/segment-0a gpdb=${new_tag}
+kubectl set image pod/segment-0b gpdb=${new_tag}
 # wait for the kubectl to have all the pods restarted
 watch kubectl get pods
 # reset dns on master
